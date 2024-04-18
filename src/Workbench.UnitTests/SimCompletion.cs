0828c19b4ee3a6ba14dced796df808572c8813f1
Oleg Shmytov <olegs@scaleoutsoftware.com>
2024-04-18 12:20:46 -0700

    Added a new RunThisTwinTest() unit test.

--- src/Workbench.UnitTests/SimCompletion.cs
+++ src/Workbench.UnitTests/SimCompletion.cs
@@ -16,6 +16,7 @@
 
 #endregion
 
+using Scaleout.DigitalTwin.DevEnv.Tests.Messages;
 using Scaleout.DigitalTwin.DevEnv.Tests.SimulatedCar;
 using Scaleout.DigitalTwin.Workbench;
 using Scaleout.Streaming.DigitalTwin.Core;
@@ -191,6 +192,68 @@ namespace Scaleout.DigitalTwin.DevEnv.Tests
                 }
             }
             Assert.Equal(4, stepCount);
+        }
+
+        class RunThisTwinSimProcessor : SimulationProcessor<SimulatedCarModel>
+        {
+            public override ProcessingResult ProcessModel(ProcessingContext context, SimulatedCarModel digitalTwin, DateTimeOffset currentTime)
+            {
+                // Delay forever when speed hits zero.
+                digitalTwin.Speed = digitalTwin.Speed - 1;
+
+                if (digitalTwin.Speed == 10 && digitalTwin.Id == "Car_Sleeper")
+                    context.SimulationController.DelayInfinitely();
+                else if (digitalTwin.Speed == 20 && digitalTwin.Id == "Car_Waker")
+                    context.SendToTwin(nameof(SimulatedCar), "Car_Sleeper", new StatusMessage() { Payload = "WakeUp!" });
+                else if (digitalTwin.Speed == 0)
+                    context.SimulationController.DelayInfinitely();
+
+                return ProcessingResult.DoUpdate;
+            }
+        }
+
+        class RunThisTwinMsgProcessor : MessageProcessor<SimulatedCarModel, StatusMessage>
+        {
+            public override ProcessingResult ProcessMessages(ProcessingContext context, SimulatedCarModel digitalTwin, IEnumerable<StatusMessage> newMessages)
+            {
+                foreach (var message in newMessages)
+                {
+                    digitalTwin.Status = message.Payload;
+                    if (message.Payload == "WakeUp!")
+                        context.SimulationController.RunThisTwin();
+                }
+                return ProcessingResult.DoUpdate;
+            }
+        }
+
+        [Fact]
+        public void RunThisTwinTest()
+        {
+            SimulationWorkbench env = new SimulationWorkbench(logger: null);
+            env.AddSimulationModel(nameof(SimulatedCar), new RunThisTwinSimProcessor(), new RunThisTwinMsgProcessor());
+
+            var car1 = new SimulatedCarModel { Speed = 30 };
+            env.AddInstance("Car_Waker", nameof(SimulatedCar), car1);
+            var car2 = new SimulatedCarModel { Speed = 15 };
+            env.AddInstance("Car_Sleeper", nameof(SimulatedCar), car2);
+
+            DateTime startTime = new DateTime(2023, 1, 1);
+            env.InitializeSimulation(startTime,
+                      endTime: DateTimeOffset.MaxValue,
+                      simulationIterationInterval: TimeSpan.FromSeconds(1));
+
+
+            int stepCount = 0;
+            for (; stepCount < 100; stepCount++) // should only take 29 steps
+            {
+                var stepResult = env.Step();
+                if (stepResult.SimulationStatus != SimulationStatus.Running)
+                {
+                    Assert.Equal(SimulationStatus.NoRemainingWork, stepResult.SimulationStatus);
+                    break;
+                }
+            }
+            Assert.Equal(29, stepCount);
         }
     }
 }
