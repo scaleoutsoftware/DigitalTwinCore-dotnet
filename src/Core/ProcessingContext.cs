3a46e80b9a7698e7f3832dbdfacf488f04c356cd
Mark Waterman <markwaterman@users.noreply.github.com>
2024-10-21 16:45:48 -0700

    Add ProcessingContext SendAlert() overload that uses the model-defined alert provider instead of requiring a named provider arg.

--- src/Core/ProcessingContext.cs
+++ src/Core/ProcessingContext.cs
@@ -21,33 +21,33 @@ using System.Collections.Generic;
 
 namespace Scaleout.Streaming.DigitalTwin.Core
 {
-	/// <summary>
-	/// Message processing context for currently processing messages.
-	/// It allows sending messages back to a data source where the messages 
-	/// are originated from.
-	/// </summary>
-	public abstract class ProcessingContext
-	{
-		/// <summary>
-		/// Data source unique identifier, e.g. for IoT use case it is typically device Id,
-		/// which is also used as a digital twin Id.
-		/// </summary>
-		public abstract string DataSourceId { get; }
+    /// <summary>
+    /// Message processing context for currently processing messages.
+    /// It allows sending messages back to a data source where the messages 
+    /// are originated from.
+    /// </summary>
+    public abstract class ProcessingContext
+    {
+        /// <summary>
+        /// Data source unique identifier, e.g. for IoT use case it is typically device Id,
+        /// which is also used as a digital twin Id.
+        /// </summary>
+        public abstract string DataSourceId { get; }
 
-		/// <summary>
-		/// Digital twin model type.
-		/// </summary>
-		public abstract string DigitalTwinModel { get; }
+        /// <summary>
+        /// Digital twin model type.
+        /// </summary>
+        public abstract string DigitalTwinModel { get; }
 
-		/// <summary>
-		/// Sends a message back to a data source origination point (e.g. IoT device). When sending 
-		/// a message to the ScaleOut Messaging REST service as a data source, the message content
-		/// must be JSON encoded.
-		/// </summary>
-		/// <param name="message">JSON encoded message as <see cref="T:byte[]" />.</param>
-		/// <returns><see cref="SendingResult.Enqueued"/> when message was successfully enqueued,
-		/// <see cref="SendingResult.NotHandled"/> otherwise.</returns>
-		public abstract SendingResult SendToDataSource(byte[] message);
+        /// <summary>
+        /// Sends a message back to a data source origination point (e.g. IoT device). When sending 
+        /// a message to the ScaleOut Messaging REST service as a data source, the message content
+        /// must be JSON encoded.
+        /// </summary>
+        /// <param name="message">JSON encoded message as <see cref="T:byte[]" />.</param>
+        /// <returns><see cref="SendingResult.Enqueued"/> when message was successfully enqueued,
+        /// <see cref="SendingResult.NotHandled"/> otherwise.</returns>
+        public abstract SendingResult SendToDataSource(byte[] message);
 
         /// <summary>
         /// Sends a message back to a data source origination point (e.g. IoT device). When sending 
@@ -127,13 +127,21 @@ namespace Scaleout.Streaming.DigitalTwin.Core
         /// <param name="message">The user message to log.</param>
         public abstract void LogMessage(LogSeverity severity, string message);
 
-		/// <summary>
-		/// Sends an alert to a given Alerting provider. 
+        /// <summary>
+        /// Sends an alert using the specified alerting provider. 
+        /// </summary>
+        /// <param name="providerName">The name of the configuration of an Alerting Provider to send the alert to.</param>
+        /// <param name="alertMessage">The provided object contains information about the alert data, as well as the provider to target.</param>
+        /// <returns></returns>
+        public abstract SendingResult SendAlert(string providerName, AlertMessage alertMessage);
+
+        /// <summary>
+		/// Sends an alert using the alerting provider that is associated with the 
+        /// digital twin model.
 		/// </summary>
-		/// <param name="providerName">The name of the configuration of an Alerting Provider to send the alert to.</param>
 		/// <param name="alertMessage">The provided object contains information about the alert data, as well as the provider to target.</param>
 		/// <returns></returns>
-		public abstract SendingResult SendAlert(string providerName, AlertMessage alertMessage);
+		public abstract SendingResult SendAlert(AlertMessage alertMessage);
 
         /// <summary>
         /// Returns the reference to registered persistence provider the model is using.
@@ -149,33 +157,33 @@ namespace Scaleout.Streaming.DigitalTwin.Core
         /// </summary>
         public abstract Dictionary<string, IAnomalyDetectionProvider> AnomalyDetectionProviders { get; }
 
-		/// <summary>
-		/// Starts a new timer for the digital twin <see cref="DataSourceId"/>.
-		/// </summary>
-		/// <param name="timerName">The timer name.</param>
-		/// <param name="interval">The timer interval.</param>
-		/// <param name="type">The type of the timer.</param>
-		/// <param name="timerCallback">A delegate representing a user-defined timer callback static method to be executed.</param>
-		/// <returns><see cref="TimerActionResult.Success"/> if the timer was started successfully, otherwise one of the following 
-		/// error codes is returned: <see cref="TimerActionResult.FailedTooManyTimers"/> when the maximum number of timers is reached or 
-		/// <see cref="TimerActionResult.FailedInternalError"/> if an error occurred during the method call.</returns>
-		public abstract TimerActionResult StartTimer(string timerName, TimeSpan interval, TimerType type, TimerHandler timerCallback);
+        /// <summary>
+        /// Starts a new timer for the digital twin <see cref="DataSourceId"/>.
+        /// </summary>
+        /// <param name="timerName">The timer name.</param>
+        /// <param name="interval">The timer interval.</param>
+        /// <param name="type">The type of the timer.</param>
+        /// <param name="timerCallback">A delegate representing a user-defined timer callback static method to be executed.</param>
+        /// <returns><see cref="TimerActionResult.Success"/> if the timer was started successfully, otherwise one of the following 
+        /// error codes is returned: <see cref="TimerActionResult.FailedTooManyTimers"/> when the maximum number of timers is reached or 
+        /// <see cref="TimerActionResult.FailedInternalError"/> if an error occurred during the method call.</returns>
+        public abstract TimerActionResult StartTimer(string timerName, TimeSpan interval, TimerType type, TimerHandler timerCallback);
 
-		/// <summary>
-		/// Stops the specified timer.
-		/// </summary>
-		/// <param name="timerName">The timer name.</param>
-		/// <returns><see cref="TimerActionResult.Success"/> if the timer was stopped successfully, otherwise one of the following 
-		/// error codes is returned: <see cref="TimerActionResult.FailedNoSuchTimer"/> when the specified timer was not found or 
-		/// <see cref="TimerActionResult.FailedInternalError"/> if an error occurred during the method call.</returns>
-		public abstract TimerActionResult StopTimer(string timerName);
+        /// <summary>
+        /// Stops the specified timer.
+        /// </summary>
+        /// <param name="timerName">The timer name.</param>
+        /// <returns><see cref="TimerActionResult.Success"/> if the timer was stopped successfully, otherwise one of the following 
+        /// error codes is returned: <see cref="TimerActionResult.FailedNoSuchTimer"/> when the specified timer was not found or 
+        /// <see cref="TimerActionResult.FailedInternalError"/> if an error occurred during the method call.</returns>
+        public abstract TimerActionResult StopTimer(string timerName);
 
-		/// <summary>
-		/// Returns the <see cref="ISimulationController"/> interface to control
-		/// all aspects of model simulation.
-		/// </summary>
-		/// <returns>Get the <see cref="ISimulationController"/> interface.</returns>
-		public abstract ISimulationController SimulationController { get; }
+        /// <summary>
+        /// Returns the <see cref="ISimulationController"/> interface to control
+        /// all aspects of model simulation.
+        /// </summary>
+        /// <returns>Get the <see cref="ISimulationController"/> interface.</returns>
+        public abstract ISimulationController SimulationController { get; }
 
         /// <summary>
         /// Returns the current time in UTC. If model simulation is active the method
